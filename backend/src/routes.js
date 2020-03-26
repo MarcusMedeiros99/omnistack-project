cc4ef3d087a806c6db829a6462b424c6f9df8e86
MarcusMedeiros99 <marcuspara66@usp.br>
2020-03-26 01:18:16 -0300

    Back-end

---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const OngController  = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController')
+
+const crypto = require('crypto');
+const routes = express.Router();
+
+const connection = require('./database/connection');
+
+routes.get('/login', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post("/ongs", OngController.create );
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index)
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
